c91dfa522a5dad0f94880f1d6ae7a9ce4a87ef6b
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2024-05-11 23:50:35 +0530

    update

--- Searching And Sorting/Totaloccr.cpp
+++ Searching And Sorting/Totaloccr.cpp
@@ -2,13 +2,37 @@
 #include<vector>
 #include<bits/stdc++.h>
 using namespace std;
-int binary(vector<int>& vp,int target){
-    int count=0;
+int Last(vector<int>& vp,int target){
+       int ans=-1;
        int s=0,e=vp.size()-1;
        int mid=s+(e-s)/2;
        while(s<=e){
         if(target==vp[mid]){
-            count++;
+            ans=mid;
+            s=mid+1;
+        }
+        else if(target<vp[mid]){
+           e=mid-1;
+        }
+         else if(target>vp[mid]){
+           s=mid+1;
+        }
+        mid=s+(e-s)/2;
+       }
+       if(ans>=0){
+        return ans;
+       }
+       else{
+        return -1;
+       }
+}
+int first(vector<int>& vp,int target){
+       int ans=-1;
+       int s=0,e=vp.size()-1;
+       int mid=s+(e-s)/2;
+       while(s<=e){
+        if(target==vp[mid]){
+            ans=mid;
             e=mid-1;
         }
         else if(target<vp[mid]){
@@ -19,15 +43,21 @@ int binary(vector<int>& vp,int target){
         }
         mid=s+(e-s)/2;
        }
-}
+       if(ans>=0){
+        return ans;
+       }
+       else{
+        return -1;
+       }}
 int main()
 {
     int target;
     cin>>target;
-    vector<int>vp{1,2,3,4,5,6,7,8,9,15,11};
+    vector<int>vp{1,2,3,4,4,4,7,8,9,15,11};
     sort(vp.begin(),vp.end());
-    int n=binary(vp,target);
-    cout<<n<<endl;
+    int n=first(vp,target);
+    int m=Last(vp,target);
+    cout<<m-n+1;
     
  return 0;
 }
